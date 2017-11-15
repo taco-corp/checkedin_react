213e165c4684b899498eef2be04c476c0f546c3c
jonriddick <jonathansyerriddick@gmail.com>
2017-11-15 18:10:05 -0500

    building event page front end

--- client/src/pages/Event.js
+++ client/src/pages/Event.js
@@ -8,43 +8,52 @@ class Event extends Component {
     return (
       
       <div className="App">
-        <div className="col-md-12 thumbnail">
-          <p>
+        <row className="col-md-12 thumbnail">
             <h1>Event Title Goes Here</h1>
-          </p>
-        </div>
-        <div className="col-md-8 bottom">
-        {
-          Object.keys(temp).map((id)=>{
-            console.log(id)
-            return(
-              
-                <div className="col-md-4" key={id}>
-                  <div className="thumbnail">
-                  <h3>{temp[id].first_name + ' ' + temp[id].last_name}</h3>
-                  <a className="pic" href={temp[id].profile_url} ><img className = "rounded " src={temp[id].picture} alt=""/></a>
+        </row>
+        <row className="col-md-12 thumbnail">
+          <div id="eventcheckinbtn" class="container col-md-4 col-md-offset-4">
+            <a class="btn btn-primary btn-lg btn-block" href="/auth/linkedin/{{key.sanitized_event_name}}" id="submit2" width="300">Get CheckedIn with LinkedIn</a>
+          </div>
+        </row>
+        <row>
+          <div className="col-md-8 bottom">
+            <div className="thumbnail">
+              <h1>Event Guests</h1>
+            </div>
+          {
+            Object.keys(temp).map((id)=>{
+              console.log(id)
+              return(
                 
-                  <a className='btn btn-primary' href={temp[id].profile_url}>
-                  Visit on LinkedIn
-                  </a>
-
-              
+                  <div className="col-md-4" key={id}>
+                    <div className="thumbnail">
+                      <h3>{temp[id].first_name + ' ' + temp[id].last_name}</h3>
+                      <a className="pic" href={temp[id].profile_url} ><img className = "rounded " src={temp[id].picture} alt=""/></a>
+                    
+                      <a className='btn btn-primary' href={temp[id].profile_url}>
+                        Visit on LinkedIn
+                      </a>
+                    </div>
                   </div>
               
-                </div>
-             
-            )
-          })
-        }
-        </div>
-        <div className="col-md-4 thumbnail">
-        <p> <h3>
-          Event Host: Jon Riddick<br/>
-          Event Location: Raleigh<br/>
-          Event Time: 4pm<br/>
-          Event Description: Learning React<br/>
-        </h3></p>
-        </div>
+              )
+            })
+          }
+          </div>
+          <div className="col-md-4 thumbnail">
+            <h1>Event Information</h1>
+            
+            <div>
+            <h3>
+              Event Host: Jon Riddick<br/>
+              Event Location: Raleigh<br/>
+              Event Time: 4pm<br/>
+              Event Description: Learning React<br/>
+            </h3>
+            </div>
+          </div>
+        </row>
       </div>
     );
   }

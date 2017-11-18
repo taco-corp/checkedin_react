29fceb1f0dae10530343b84e7081c436ad42aaf8
Zeynep Dayar <zeynep.dayar@gmail.com>
2017-11-18 15:15:15 -0500

    A couple of small wording changes

--- client/src/components/HistoryOfEvents/HistoryOfEvents.js
+++ client/src/components/HistoryOfEvents/HistoryOfEvents.js
@@ -4,9 +4,9 @@ import "./HistoryOfEvents.css";
 const HistoryOfEvents = () => 
     <div className="col-md-5 col-md-offset-2 bottom thumbnail text-center">
         <div className="thumbnail text-center">
-        <h3>History of Events</h3>
+        <h3>List of Events</h3>
         </div>
         <h4>Bark in the Park  |  11/06/17  |  Hosted by: Dallas Bro'Pitbull</h4>
-    </div>
+    </div>;
 
 export default HistoryOfEvents;

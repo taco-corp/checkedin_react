0a4d3227ebfe15e5771a19cbafa61e6ce7e35167
jonriddick <jonathansyerriddick@gmail.com>
2017-11-18 14:27:05 -0500

    component work

---
+++ client/src/components/Attendee/Attendee.js
@@ -0,0 +1,20 @@
+import React from "react";
+import temp from '../../snippet'
+import "./Attendee.css";
+
+const Attendee = (props) => 
+    <div className="col-md-4" key={props.id}>
+        <div className="thumbnail">
+        <h3>{temp[props.id].first_name + ' ' + temp[props.id].last_name}</h3>
+        <a className="pic" href={temp[props.id].profile_url} ><img className = "rounded " src={temp[props.id].picture} alt=""/></a>
+        
+        <a className='btn btn-primary' href={temp[props.id].profile_url}>
+            Visit on LinkedIn
+        </a><br/>
+        <a className='btn btn-primary' href={temp[props.id].profile_url}>
+            Save Me
+        </a>
+        </div>
+    </div>
+
+export default Attendee;
--- client/src/pages/Event.js
+++ client/src/pages/Event.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import '../App.css';
 import temp from '../snippet'
+import EventInfo from "../components/EventInfo";
+import EventGuests from "../components/EventGuests";
 
 
 class Event extends Component {
@@ -8,51 +10,19 @@ class Event extends Component {
     return (
       
       <div className="App">
-        <row className="col-md-12 thumbnail">
-            <h1>Event Title Goes Here</h1>
-        </row>
-        <row className="col-md-12 thumbnail">
-          <div id="eventcheckinbtn" class="container col-md-4 col-md-offset-4">
-            <a class="btn btn-primary btn-lg btn-block" href="/auth/linkedin/{{key.sanitized_event_name}}" id="submit2" width="300">Get CheckedIn with LinkedIn</a>
-          </div>
-        </row>
+        <div className="thumbnail col-md-8 col-md-offset-2">
+          <row className="">
+              <h1>Bark in the Park</h1>
+          </row>
+          <row className="col-md-12">
+            <div id="eventcheckinbtn" class="container col-md-4 col-md-offset-4">
+              <a class="btn btn-primary btn-lg btn-block" href="/auth/linkedin/{{key.sanitized_event_name}}" id="submit2" width="300">Get CheckedIn with LinkedIn</a>
+            </div>
+          </row>
+        </div>
         <row>
-          <div className="col-md-8 bottom">
-            <div className="thumbnail">
-              <h1>Event Guests</h1>
-            </div>
-          {
-            Object.keys(temp).map((id)=>{
-              console.log(id)
-              return(
-                
-                  <div className="col-md-4" key={id}>
-                    <div className="thumbnail">
-                      <h3>{temp[id].first_name + ' ' + temp[id].last_name}</h3>
-                      <a className="pic" href={temp[id].profile_url} ><img className = "rounded " src={temp[id].picture} alt=""/></a>
-                    
-                      <a className='btn btn-primary' href={temp[id].profile_url}>
-                        Visit on LinkedIn
-                      </a>
-                    </div>
-                  </div>
-              
-              )
-            })
-          }
-          </div>
-          <div className="col-md-4 thumbnail">
-            <h1>Event Information</h1>
-            
-            <div>
-            <h3>
-              Event Host: Jon Riddick<br/>
-              Event Location: Raleigh<br/>
-              Event Time: 4pm<br/>
-              Event Description: Learning React<br/>
-            </h3>
-            </div>
-          </div>
+          <EventGuests />
+          <EventInfo />
         </row>
       </div>
     );

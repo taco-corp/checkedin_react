643867ff7fe51efc08b35eecda82791260043825
jonriddick <jonathansyerriddick@gmail.com>
2017-11-19 15:36:57 -0500

    More component work

---
+++ client/src/components/EventHeader/EventHeader.js
@@ -0,0 +1,18 @@
+import React from "react";
+import temp from '../../snippet'
+import "./EventHeader.css";
+import Attendee from '../Attendee'
+
+const EventHeader = () => 
+    <div className="thumbnail col-md-8 col-md-offset-2">
+        <row className="">
+            <h1>Bark in the Park</h1>
+        </row>
+        <row className="col-md-12">
+        <div id="eventcheckinbtn" class="container col-md-4 col-md-offset-4">
+            <a class="btn btn-primary btn-lg btn-block" href="/auth/linkedin/{{key.sanitized_event_name}}" id="submit2" width="300">Get CheckedIn with LinkedIn</a>
+        </div>
+        </row>
+    </div>
+
+export default EventHeader;
--- client/src/pages/Event.js
+++ client/src/pages/Event.js
@@ -3,6 +3,8 @@ import '../App.css';
 import temp from '../snippet'
 import EventInfo from "../components/EventInfo";
 import EventGuests from "../components/EventGuests";
+import EventHeader from "../components/EventHeader";
+import Row from "../components/Row";
 
 
 class Event extends Component {
@@ -10,20 +12,11 @@ class Event extends Component {
     return (
       
       <div className="App">
-        <div className="thumbnail col-md-8 col-md-offset-2">
-          <row className="">
-              <h1>Bark in the Park</h1>
-          </row>
-          <row className="col-md-12">
-            <div id="eventcheckinbtn" class="container col-md-4 col-md-offset-4">
-              <a class="btn btn-primary btn-lg btn-block" href="/auth/linkedin/{{key.sanitized_event_name}}" id="submit2" width="300">Get CheckedIn with LinkedIn</a>
-            </div>
-          </row>
-        </div>
-        <row>
+        <EventHeader />
+        <Row>
           <EventGuests />
           <EventInfo />
-        </row>
+        </Row>
       </div>
     );
   }

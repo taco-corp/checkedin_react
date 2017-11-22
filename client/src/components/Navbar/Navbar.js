125236807179612092600f65b7e0d193b4e45a65
jonriddick <jonathansyerriddick@gmail.com>
2017-11-21 20:15:06 -0500

    Working on Navbar & login page upon landing

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -7,17 +7,19 @@ const Navbar = props =>
   <nav className="navbar navbar-default fixed-top navbar-toggleable-md navbar-light bg-faded">
     <div className="container-fluid">
       <div className="navbar-header">
-        <Link className="navbar-brand" to="/home">
+        <Link className="navbar-brand" to="/login">
           CheckedIn
         </Link>
       </div>
       <ul className="nav navbar-nav">
-        <li className={window.location.pathname === "/login" ? "active" : ""}>
+        <li className={
+          window.location.pathname === "/" ||
+          window.location.pathname === "/login" ? "active" : ""}>
           <Link to="/login">Login</Link>
         </li>
         <li
           className={
-            window.location.pathname === "/" ||
+            // window.location.pathname === "/" ||
             window.location.pathname === "/home"
               ? "active"
               : ""
@@ -32,19 +34,6 @@ const Navbar = props =>
           className={window.location.pathname === "/event" ? "active" : ""}>
           <Link to="/event">Event</Link>
         </li>
-          <div class="pull-right">
-              <form class="navbar-form" role="search">
-            <div class="input-group">
-                <input type="text" class="form-control" placeholder="Search" name="srch-term" id="srch-term"/>
-              <div class="input-group-btn">
-                  <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
-              </div>
-            </div>
-              </form>
-          </div>
-    {/* </div><!--/.nav-collapse --> */}
-        
-        
       </ul>
     </div>
   </nav>;

f5fc26e668893873d384b2328514fd414532db5a
Kristin Glaze <ikglaze33@gmail.com>
2017-11-25 19:13:44 -0500

    Made use of componentDidMount function and states to display hard-coded user events.

--- client/src/components/HistoryOfEvents/HistoryOfEvents.js
+++ client/src/components/HistoryOfEvents/HistoryOfEvents.js
@@ -1,10 +1,22 @@
 import "./HistoryOfEvents.css";
 import { List, ListItem } from "../../components/List";
 import React, { Component } from "react";
-var eventsHistory = [{"id": "4", "title": "Ball in the Park", "date": "11/11/11", "host": "Dallas Bro'Pitbull"},
+import API from "../../utils/API";
+
+var eventsHistoryTemp = [{"id": "4", "title": "Ball in the Park", "date": "11/11/11", "host": "Dallas Bro'Pitbull"},
 {"id": "5", "title": "Title", "date": "03/11/11", "host": "Angela"}];
 
 class HistoryOfEvents extends Component {
+  state = {
+    eventsHistory: []
+  };
+
+  // When the component mounts
+  componentDidMount() {
+  	API.getUserInfo();
+
+    this.setState({eventsHistory: eventsHistoryTemp});
+  }
 render() {
 	return (
     <div className="col-md-5 col-md-offset-2 bottom thumbnail text-center">
@@ -14,7 +26,7 @@ render() {
         <a href="/event"> <h4>Bark in the Park  |  11/06/17  |  Hosted by: Dallas Bro'Pitbull</h4></a>
             <List>
             {
-              eventsHistory.map(eventHistory => {
+              this.state.eventsHistory.map(eventHistory => {
               return (
                 <ListItem key={eventHistory.id}>
                   <a href="#"><p>{eventHistory.title} | {eventHistory.date} | Hosted by: {eventHistory.host}</p></a>

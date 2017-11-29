64b6b7a3b0c6eb75ba040d6d1abdf0c951b76408
jonriddick <jonathansyerriddick@gmail.com>
2017-11-29 16:42:07 -0500

    fixed className issue

--- client/src/components/NewEvent/NewEvent.js
+++ client/src/components/NewEvent/NewEvent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import "./NewEvent.css";
+import Row from "../Row"
 
 // const NewEvent = () => 
 
@@ -13,12 +14,12 @@ class NewEvent extends Component {
         return (
             <div className="App text-center">
                 <div className="thumbnail col-md-4 col-md-offset-2">
-                <row className="col-md-12">
-                    <div id="createEventBtn" class="container col-md-8 col-md-offset-3">
+                <Row className="col-md-12">
+                    <div id="createEventBtn" className="container col-md-8 col-md-offset-3">
                     <button className="btn btn-primary btn-block" id="createEvent"
                             onClick={this.redirectToEventCreation}>Create New Event</button>
                     </div>
-                </row>
+                </Row>
                 </div>
             </div>
         );

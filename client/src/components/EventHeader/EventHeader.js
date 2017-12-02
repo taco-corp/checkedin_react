b9514db1f4b43b63a996ad3a978d79c641ed406e
jonriddick <jonathansyerriddick@gmail.com>
2017-12-01 22:35:02 -0500

    quick fix :)

--- client/src/components/EventHeader/EventHeader.js
+++ client/src/components/EventHeader/EventHeader.js
@@ -9,7 +9,6 @@ class EventHeader extends Component {
   }
 
   handleCheckin = event => {
-      //event.preventDefault();
       let checkinObject = {
         eventId: this.props.eventId,
         displayName: localStorage.getItem('displayName'),
@@ -18,18 +17,10 @@ class EventHeader extends Component {
         profileURL: localStorage.getItem('profileURL')
       };
       API.addUserToEvent(checkinObject);
-      // The following line adds the checkin user to the users collection if they don't already exist
-      //window.location='http://localhost:8080/api/auth/linkedin';
-      this.forceUpdate();
     };
   
     render() {
-      console.log("event header props" , this.props)
-      //We need the eventName to be dynamic and dependant upong the URL
-      // var eventName = 'http://localhost:3000/event/:eventName';
-      //var eventName = req.params.eventName
-      //var eventName = 'Bark in the Park';
-
+     
       return (
         <div className="thumbnail center-text col-md-6 col-md-offset-3">
              <h2 id="eventTitle" className="text-center">{this.props.event}</h2>

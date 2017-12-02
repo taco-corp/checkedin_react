f7a1d1ed06cea01f4993b27c29da1157af201bf1
jonriddick <jonathansyerriddick@gmail.com>
2017-12-01 22:34:18 -0500

    Deleted code mostly

--- client/src/components/Attendee/Attendee.js
+++ client/src/components/Attendee/Attendee.js
@@ -5,47 +5,13 @@ import "./Attendee.css";
 const Attendee = (props) => 
     <div className="col-md-4" key={props.id}>
         <div className="thumbnail">
-        <h3>{temp[props.id].first_name + ' ' + temp[props.id].last_name}</h3>
-        <a ClassName="pic" id="image" href={temp[props.id].profile_url} ><img className = "rounded " src={temp[props.id].picture} alt=""/></a>
-        
-        <a className='btn btn-primary' href={temp[props.id].profile_url}>
-            Visit on LinkedIn
-        </a><br/>
-        {/* <a className='btn btn-primary' href={temp[props.id].profile_url}>
-            Save Me
-        </a> */}
+            <h3>{temp[props.id].first_name + ' ' + temp[props.id].last_name}</h3>
+            <a ClassName="pic" id="image" href={temp[props.id].profile_url} ><img className = "rounded " src={temp[props.id].picture} alt=""/></a>
+            
+            <a className='btn btn-primary' href={temp[props.id].profile_url}>
+                Visit on LinkedIn
+            </a><br/>
         </div>
     </div>
 
-
-
-
-// class Attendee extends Component {
-//     constructor(props){
-//         super(props)
-//     }
-//     render(){
-//         let temp = this.props.temp;
-//         let id = this.props.id;
-//         console.log("Attendee Component", temp)
-//         console.log("Attendee Component", id)
-        
-//         //let props = this.props.id
-//         return(
-//             <div className="col-md-4" key={id}>
-//                 <div className="thumbnail">
-//                 <h3>{temp[id].first_name + ' ' + temp[id].last_name + temp[id].displayName}</h3>
-//                 <a ClassName="pic" id="image" href={temp[id].profileURL} ><img className = "rounded " src={temp[id].picture} alt=""/></a>
-                
-//                 <a className='btn btn-primary' href={temp[id].profileURL}>
-//                     Visit on LinkedIn
-//                 </a><br/>
-//                 {/* <a className='btn btn-primary' href={temp[props.id].profile_url}>
-//                     Save Me
-//                 </a> */}
-//                 </div>
-//             </div>
-//         )
-//     }
-// }
 export default Attendee;

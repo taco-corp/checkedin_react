7e3527f3e71595c6a06b21635c3cd4d74344f684
jonriddick <jonathansyerriddick@gmail.com>
2017-12-01 22:32:13 -0500

    Moved this content to the Event.js page, also deleted some code

--- client/src/components/EventHeader/EventHeader.js
+++ client/src/components/EventHeader/EventHeader.js
@@ -8,22 +8,7 @@ class EventHeader extends Component {
     super(props)
   }
 
-    // handleCheckin = event =>{
-    //   event.preventDefault();
-    //   let checkinObject = {
-    //     displayName: localStorage.getItem('displayName'),
-    //     id: localStorage.getItem('id'),
-    //     picture: localStorage.getItem('picture'),
-    //     profileURL: localStorage.getItem('profileURL')
-    //   }
-    //   API.addUserToEvent(checkinObject, this.props.userObject)
-    // };
-  
-        //Removed from handleLogin function
-        //let userId = localStorage.getItem("id")
-        //API.addUserToEvent(userId, this.props.eventId)
-
-    handleCheckin = event => {
+  handleCheckin = event => {
       //event.preventDefault();
       let checkinObject = {
         eventId: this.props.eventId,
@@ -37,7 +22,6 @@ class EventHeader extends Component {
       //window.location='http://localhost:8080/api/auth/linkedin';
       this.forceUpdate();
     };
-
   
     render() {
       console.log("event header props" , this.props)
@@ -50,24 +34,11 @@ class EventHeader extends Component {
         <div className="thumbnail center-text col-md-6 col-md-offset-3">
              <h2 id="eventTitle" className="text-center">{this.props.event}</h2>
               <div className="col-md-6 col-md-offset-3">
-          <button className="btn btn-primary btn-lg btn-block" id="login" onClick={this.handleCheckin}>CheckIn with LinkedIn</button>
+          <button className="btn btn-primary btn-lg btn-block" id="login" onClick={this.handleCheckin.bind(this)}>CheckIn with LinkedIn</button>
           </div>
         </div>
       );
     }
   }
-
-
-// const EventHeader = () => 
-//     <div className="thumbnail col-md-8 col-md-offset-2">
-//         <row className="">
-//             <h1>Bark in the Park</h1>
-//         </row>
-//         <row>        
-//         <div id="eventcheckinbtn" class="container col-md-4 col-md-offset-4">
-//             <a class="btn btn-primary btn-lg btn-block" id="submit2" width="300">Get CheckedIn with LinkedIn</a>
-//         </div>
-//         </row>
-//     </div>
 
 export default EventHeader;
